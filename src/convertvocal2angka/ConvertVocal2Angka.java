0bcd37323baba0241d9ff95ca7d1d1b4c0e134e6
User <User@LAPTOP-NNIHKQG5>
2018-08-13 20:45:26 +0700

    initial commit

---
+++ src/convertvocal2angka/ConvertVocal2Angka.java
@@ -0,0 +1,16 @@
+package convertvocal2angka;
+
+public class ConvertVocal2Angka {
+    public static void tampilJudul(String identitas) {
+        System.out.println("Identitas : " + identitas);
+        
+        System.out.println("\nConvert Kalimat Alay Angka (Vokal Ke Angka)\n");
+        
+    }
+    public static void main(String[] args){
+        String identitas = "Syerli Trinadya / XRPL2 /33";
+        
+        tampilJudul(identitas);
+    }
+    
+}
