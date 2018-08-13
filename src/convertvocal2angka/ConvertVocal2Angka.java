7b2696293af1ec1a6dd6b6e6f17c180f6926dd44
User <User@LAPTOP-NNIHKQG5>
2018-08-13 20:49:27 +0700

    Tambah konversi

--- src/convertvocal2angka/ConvertVocal2Angka.java
+++ src/convertvocal2angka/ConvertVocal2Angka.java
@@ -12,6 +12,7 @@ public class ConvertVocal2Angka {
         
         tampilJudul(identitas);
         String kalimat = tampilInput();
+        String convert = vocal2Angka(kalimat);
     }
     private static String tampilInput() {
         Scanner scanner = new Scanner(System.in);
@@ -21,6 +22,19 @@ public class ConvertVocal2Angka {
         System.out.println("Kalimat Asli: "+ kalimat);
         
         return kalimat;
+    }
+    private static String vocal2Angka(String kalimat) {
+        char [][] arConvert = 
+        {{'a','4'},{'i','1'},{'u','2'},{'e','3'},{'o','0'}};
+        
+        kalimat = kalimat.toLowerCase();
+        for(int i=0;i<arConvert.length;i++)
+            kalimat = kalimat.replace(arConvert[i][0], arConvert[i][1]);
+        
+        return kalimat;
+                
+    }
+
 
     
 }
